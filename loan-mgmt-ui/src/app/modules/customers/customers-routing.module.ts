0fad4ee235586ccd97724776892a3e52a70e54a9
Anshu Ahirwar <Ahirwar.Anshu@con-way.com>
2023-02-26 17:43:54 +0530

    customer components added

--- loan-mgmt-ui/src/app/modules/customers/customers-routing.module.ts
+++ loan-mgmt-ui/src/app/modules/customers/customers-routing.module.ts
@@ -1,11 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddCustomerComponent } from './add-customer/add-customer.component';
+import { CustomerListComponent } from './customer-list/customer-list.component';
 import { CustomersComponent } from './customers.component';
+import { DeleteCustomerComponent } from './delete-customer/delete-customer.component';
+import { EditCustomerComponent } from './edit-customer/edit-customer.component';
+import { SearchCustomerComponent } from './search-customer/search-customer.component';
 
-const routes: Routes = [{ path: '', component: CustomersComponent }];
+const routes: Routes = [
+  // { path: '', component: CustomersComponent },
+  { path: '', component: CustomerListComponent },
+  { path: 'add', component: AddCustomerComponent },
+  { path: 'edit:id', component: EditCustomerComponent },
+  { path: 'delete:id', component: DeleteCustomerComponent },
+  { path: 'search', component: SearchCustomerComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class CustomersRoutingModule { }
+export class CustomersRoutingModule {}
--- loan-mgmt-ui/src/app/modules/customers/customers.module.ts
+++ loan-mgmt-ui/src/app/modules/customers/customers.module.ts
@@ -4,12 +4,20 @@ import { CommonModule } from '@angular/common';
 import { CustomersRoutingModule } from './customers-routing.module';
 import { CustomersComponent } from './customers.component';
 import { CustomerListComponent } from './customer-list/customer-list.component';
+import { AddCustomerComponent } from './add-customer/add-customer.component';
+import { EditCustomerComponent } from './edit-customer/edit-customer.component';
+import { DeleteCustomerComponent } from './delete-customer/delete-customer.component';
+import { SearchCustomerComponent } from './search-customer/search-customer.component';
 
 
 @NgModule({
   declarations: [
     CustomersComponent,
-    CustomerListComponent
+    CustomerListComponent,
+    AddCustomerComponent,
+    EditCustomerComponent,
+    DeleteCustomerComponent,
+    SearchCustomerComponent
   ],
   imports: [
     CommonModule,
